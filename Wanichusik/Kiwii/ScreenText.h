b1673e20829f4a0f6541a6749b6995817bd85885
Ivan-Kr <kiwii6963@gmail.com>
2021-11-19 19:37:07 +0200

    made hash folder and interpre

--- Wanichusik/Kiwii/ScreenText.h
+++ Wanichusik/Kiwii/ScreenText.h
@@ -1,25 +1,40 @@
 #pragma once
 #include "Kiwii.h"
+#include <iostream>
 #include <windows.h>
 #include <stdio.h>
 
 typedef wchar_t CHARR;
 
+void SetWindow(int Width, int Height)
+{
+	_COORD coord;
+	coord.X = Width;
+	coord.Y = Height;
+	_SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = Height - 1;
+	Rect.Right = Width - 1;
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleScreenBufferSize(Handle, coord);
+	SetConsoleWindowInfo(Handle, TRUE, &Rect);
+}
+
 class ScreenText :
     public Kiwii
 {
-	WORD Width;
-	WORD Height;
+	WORD width;
+	WORD height;
 
-	CHARR* screen;
 	HANDLE hConsole;
 	DWORD dwBytesWritten = 0;
+
+	CHARR gradient[256];
 public:
-    ScreenText(WORD width, WORD height);
-
-	void Write(unsigned __int32 index, CHARR sym);
-	CHARR Read(unsigned __int32 index);
-
-	void Out();
+	ScreenText(WORD Width, WORD Height,BYTE font_size[2]) {
+		hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+		SetConsoleActiveScreenBuffer(hConsole);
+	}
 };
 
--- Wanichusik/Wanichusik.cpp
+++ Wanichusik/Wanichusik.cpp
@@ -1,50 +1,12 @@
-﻿#include "Kiwii/Screen.h"
-#include "Kiwii/Music.h"
-#include "Kiwii/Time Act.h"
-#include <string>
+﻿#include "Kiwii/Music.h"
 
-BYTE noise(WORD x, WORD y, BYTE val) {
-	BYTE vl = Rand 4;
-	switch (vl) {
-	case 0:	return 0x00;
-	case 1:	return 0x88;
-	case 2:	return 0x77;
-	case 3:	return 0xff;
-	}
-
-}
-
-double timer = 0;
 int main(int args,char* argv[])
 {
 	printf("%s\n",argv[0]);
-	//is_sound(L".\\Pocket\\Introduce.wav");
 
-	srand(time(NULL));
 
-	scene a;
+	play_test_song();
 
-	double rps=0;
-	TIME_POINT end=NOW_T, beg=NOW_T;
-	for (int i = 0;i < 128;i++) {
-		beg = NOW_T;
-		system("cls");
-		a.write_modific_scene(20, 20, true,noise);
-		Sleep(rps);
-		end = NOW_T;
-		a.write_comment(
-			"Renders = \t" + std::to_string(i) + " iter\n"+
-			"Speed render = \t" + std::to_string(time_rps_g(beg, end)) + " RPS\n"+
-			"Delay = \t" + std::to_string(1000 / time_rps_g(beg, end)) + " ms\n"+
-			"Timer = \t" + std::to_string(timer) + " s\n" +
-			"Avrg render = \t" + std::to_string(i/ timer) + " RPS"
-		);
-		timer += 1 / time_rps_g(beg, end);
-
-		//rps = time_rps_g(beg, end)*5;
-
-	}
 	system("pause");
-
 	return 0;
 }
