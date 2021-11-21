627857df5b24319bdbf2e513e60cacd8f960dfaa
Ivan-Kr <kiwii6963@gmail.com>
2021-11-21 10:50:41 +0200

    try to made quickly output

--- Wanichusik/Kiwii/ScreenText.h
+++ Wanichusik/Kiwii/ScreenText.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "Kiwii.h"
 #include <iostream>
-#include <windows.h>
+#include <Windows.h>
 #include <stdio.h>
-
-typedef wchar_t CHARR;
+#include <math.h>
 
 void SetWindow(int Width, int Height)
 {
@@ -21,20 +20,67 @@ void SetWindow(int Width, int Height)
 	SetConsoleWindowInfo(Handle, TRUE, &Rect);
 }
 
+int Normalize(int val, int maxx, int minn) {
+	return min(max(val, minn), maxx);
+}
+
 class ScreenText :
     public Kiwii
 {
-	WORD width;
-	WORD height;
+	WORD width=0;
+	WORD height=0;
 
 	HANDLE hConsole;
 	DWORD dwBytesWritten = 0;
 
-	CHARR gradient[256];
+	wchar_t* screen;
+
+	double aspect;
 public:
-	ScreenText(WORD Width, WORD Height,BYTE font_size[2]) {
+	ScreenText(WORD Width, WORD Height) {
 		hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 		SetConsoleActiveScreenBuffer(hConsole);
+		width = Width;
+		height = Height;
+		aspect = (double)width / height;
+		SetWindow(width, height);
+		screen = new wchar_t[width * height + 1];
 	}
+
+	void _aspect(double asp) {
+		aspect = asp;
+	}
+	double _aspect() {
+		return aspect;
+	}
+
+	int _width() {
+		return width;
+	}
+	int _height() {
+		return height;
+	}
+
+	void write(int x, int y,wchar_t pix) {
+		if (x >= 0 && x < width && y>=0 && y < height)
+			screen[y * width + x] = pix;
+	}
+
+	wchar_t read(int x, int y) {
+		if (x > 0 && x < width && y>0 && y < height)
+			return screen[y * width + x];
+		else
+			return 0;
+	}
+
+	void out() {
+		screen[width * height] = L'\0';
+		WriteConsoleOutputCharacter(hConsole, screen, width * height, { 0, 0 }, &dwBytesWritten);
+	}
+
+	~ScreenText() {
+		delete[] screen;
+	}
+
 };
 
--- Wanichusik/Wanichusik.cpp
+++ Wanichusik/Wanichusik.cpp
@@ -1,12 +1,37 @@
-﻿#include "Kiwii/Music.h"
+﻿#pragma once
+#include "Kiwii/ScreenText.h"
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(int args,char* argv[])
-{
-	printf("%s\n",argv[0]);
 
+int main(){
+	//setup
+	ScreenText ux(90,30);
 
-	play_test_song();
+	float pixelAspect = 11.0f / 24.0f;
 
-	system("pause");
+	//cycle
+	for (int t = 0;t < 2000000;t++) {
+		for (int j = 0;j < ux._height();j++) {
+			for (int i = 0;i < ux._width();i++) {
+
+				double x = (double)i / ux._width() *2-1;
+				double y = (double)j / ux._height() *2-1;
+
+				x *= ux._aspect()* pixelAspect;
+
+				x += sin(t);
+
+				wchar_t pixel = L' ';
+				if ((x * x + y * y) < 0.5) pixel = L'#';
+				ux.write(x, y, pixel);
+			}
+		}
+		ux.out();
+	}
+
+	ux.~ScreenText();
 	return 0;
 }
