0c40da08149436b036da5aa22ffd488ffc054baf
Ivan-Kr <butunska5@gmail.com>
2021-12-22 20:03:34 +0200

    i fixed table, and it's works

--- Wanichusik/Kiwii/DatoBased/Table.h
+++ Wanichusik/Kiwii/DatoBased/Table.h
@@ -14,8 +14,8 @@ namespace Kiwii {
         _Ty** _table = nullptr;
 
         ////others variables///
-        unsigned int _sx = NULL;
-        unsigned int _sy = NULL;
+        unsigned int _sx;
+        unsigned int _sy;
 
         ///////pointers////////
         bool _is_decleared = false;
@@ -23,11 +23,45 @@ namespace Kiwii {
     public:
         //////constructor//////
         Table(){}
-        void set_table(unsigned int x, unsigned int y);
+        void set_table(unsigned int x, unsigned int y) {
+            _is_decleared = true;
+
+            _sx = x;
+            _sy = y;
+
+            _table = new _Ty*[_sx];
+
+            for (unsigned int i = 0;i < _sx;i++) {
+                _table[i] = new _Ty[_sy];
+            }
+        }
 
         //////get and set//////
-        void table(unsigned int x, unsigned int y, _Ty what);
-        _Ty table(unsigned int x, unsigned int y);
+        void table(unsigned int x, unsigned int y, _Ty what) {
+            if (_is_decleared) {
+
+                if (x < _sx && y < _sy) {
+                    _table[x][y] = what;
+                }
+
+            }
+
+        }
+        _Ty table(unsigned int x, unsigned int y) {
+            if (_is_decleared) {
+
+                if (x < _sx && y < _sy) {
+                    return _table[x][y];
+                }
+                else {
+                    return _Ty();
+                }
+
+            }
+            else {
+                return _Ty();
+            }
+        }
 
 
 
@@ -64,11 +98,46 @@ namespace Kiwii {
 
         /////special func//////
 
-        void fill(_Ty what);
-        void add(_Ty what);
+        void fill(_Ty what) {
+            if (_is_decleared) {
+
+                for (unsigned int i = 0;i < _sx;i++) {
+                    for (unsigned int j = 0;j < _sy;j++) {
+                        _table[i][j] = what;
+                    }
+                }
+
+            }
+        }
+        void add(_Ty what) {
+            if (_is_decleared) {
+
+                for (unsigned int i = 0;i < _sx;i++) {
+                    for (unsigned int j = 0;j < _sy;j++) {
+                        _table[i][j] += what;
+                    }
+                }
+
+            }
+        }
 
         ///////destructor//////
-        void reset_table();
+        void reset_table() {
+            if (_is_decleared) {
+                for (unsigned int i = 0;i < _sx;i++) {
+                    delete[] _table[i];
+                }
+
+                delete[] _table;
+
+                _sx = NULL;
+                _sy = NULL;
+
+
+                _table = nullptr;
+                _is_decleared = false;
+            }
+        }
         ~Table() { 
             reset_table();
         }
