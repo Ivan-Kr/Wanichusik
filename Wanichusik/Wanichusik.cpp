f1be83b8e0d60f686ffc5319bf705e252f75176b
Ivan-Krul <kiwiivanya128@gmail.com>
2022-02-20 21:29:15 +0200

    made a perlin noise

--- Wanichusik/Wanichusik.cpp
+++ Wanichusik/Wanichusik.cpp
@@ -5,6 +5,15 @@
 
 
 int main() {
-	Kiwii_Experiment::Perlin(5, 5);
+	Kiwii_Experiments::mapf as = Kiwii_Experiments::Perlin(5, 5);
+
+	for (int i = 0;i < 5;i++) {
+		for (int j = 0;j < 5;j++)
+			std::cout<<std::hex<<uint16_t(as[i * 5 + j]);
+		std::cout << '\n';
+	}
+
+	delete[] as;
+
 	return 0;
 }
