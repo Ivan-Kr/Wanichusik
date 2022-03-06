1c5f0f5762ff75022c68ef4ca9ce4424192ab489
Ivan-Krul <kiwiivanya128@gmail.com>
2022-03-06 15:24:20 +0200

    made a connection for a top of graph

--- Wanichusik/Kiwii/DatoBased/Simulation/Experiment #0/Exp0 - Main.h
+++ Wanichusik/Kiwii/DatoBased/Simulation/Experiment #0/Exp0 - Main.h
@@ -109,6 +109,16 @@ namespace Kiwii_Experiments {
 	public:
 		Kiwii::vec2 size;
 		mapc scr;
+
+		struct top {
+			Kiwii::vec2 D;
+			std::vector<top&> Con;
+
+			void Connect(top& what) {
+				Con.push_back(what);
+			}
+		};
+
 		Exper_2(Kiwii::vec2 size) {
 			scr = new char[int(size.X * size.Y)];
 
@@ -116,8 +126,89 @@ namespace Kiwii_Experiments {
 
 			this->size = size;
 		}
-		void Segment(Kiwii::segment s) {
-
+		void Segment(int x1, int y1, int x2, int y2, int c)
+		{
+			int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+			dx = x2 - x1;
+			dy = y2 - y1;
+			dx1 = fabs(dx);
+			dy1 = fabs(dy);
+			px = 2 * dy1 - dx1;
+			py = 2 * dx1 - dy1;
+			if (dy1 <= dx1)
+			{
+				if (dx >= 0)
+				{
+					x = x1;
+					y = y1;
+					xe = x2;
+				}
+				else
+				{
+					x = x2;
+					y = y2;
+					xe = x1;
+				}
+				scr[x + y * int(size.X)] = '#';
+				for (i = 0;x < xe;i++)
+				{
+					x = x + 1;
+					if (px < 0)
+					{
+						px = px + 2 * dy1;
+					}
+					else
+					{
+						if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+						{
+							y = y + 1;
+						}
+						else
+						{
+							y = y - 1;
+						}
+						px = px + 2 * (dy1 - dx1);
+					}
+					scr[x + y * int(size.X)] = '#';
+				}
+			}
+			else
+			{
+				if (dy >= 0)
+				{
+					x = x1;
+					y = y1;
+					ye = y2;
+				}
+				else
+				{
+					x = x2;
+					y = y2;
+					ye = y1;
+				}
+				scr[x + y * int(size.X)] = '#';
+				for (i = 0;y < ye;i++)
+				{
+					y = y + 1;
+					if (py <= 0)
+					{
+						py = py + 2 * dx1;
+					}
+					else
+					{
+						if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+						{
+							x = x + 1;
+						}
+						else
+						{
+							x = x - 1;
+						}
+						py = py + 2 * (dx1 - dy1);
+					}
+					scr[x + y * int(size.X)] = '#';
+				}
+			}
 		}
 	};
 
--- Wanichusik/Wanichusik.cpp
+++ Wanichusik/Wanichusik.cpp
@@ -10,19 +10,53 @@ int main() {
 	Kiwii::Screench uv;
 	uv.setup(x, y);
 
+	Kiwii::vec2 a(0),b(1);
+
 	uint64_t t = 0;
 	while (true) {
-		if (GetAsyncKeyState(VK_ESCAPE)) { break; Sleep(100); }
+		if (GetAsyncKeyState(VK_ESCAPE)) {
+			break;
+		}
+		if (GetAsyncKeyState('W')) {
+			a.Y--;
+		}
+		if (GetAsyncKeyState('A')) {
+			a.X--;
+		}
+		if (GetAsyncKeyState('S')) {
+			a.Y++;
+		}
+		if (GetAsyncKeyState('D')) {
+			a.X++;
+		}
+		if (GetAsyncKeyState(VK_UP)) {
+			b.Y--;
+		}
+		if (GetAsyncKeyState(VK_LEFT)) {
+			b.X--;
+		}
+		if (GetAsyncKeyState(VK_DOWN)) {
+			b.Y++;
+		}
+		if (GetAsyncKeyState(VK_RIGHT)) {
+			b.X++;
+		}
+		if (GetAsyncKeyState(VK_RIGHT)) {
+			
+		}
 		if (GetAsyncKeyState(' ')) { 
-			Kiwii::vec2 a(rand()%x, rand() % y), b(rand() % x, rand() % y);
-			E.Segment(Kiwii::segment(a,b)); 
-			Sleep(10); 
+			E.Segment(a.X,a.Y,b.X,b.Y,1); 
+			Sleep(100); 
 		}
 
-
 		uv.copy(E.scr);
+
+		uv.set_screen(Kiwii::fix<int>(a.X + x * a.Y, 0, x * y),'+');
+		uv.set_screen(Kiwii::fix<int>(b.X + x * b.Y, 0, x * y), '+');
+
 		uv.insert_hex(x * y - 1, t);
 		uv.print();
+		Sleep(33);
 		t++;
 	}
 
